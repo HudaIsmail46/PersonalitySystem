a3aec8fa2883150e5a98ea14b8992ef5e51af0ba
Naim <mnaim487@gmail.com>
2020-10-22 14:15:11 +0800

    CRUD and soft delete for order done

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -10,6 +10,7 @@
         <div class="container">
             <div class="card mt-4">
                 <div class="card-header">
+                    <!-- {{$order->id}} -->
                     <p>Order Detail</p>
                 </div>
                 <div class="card-body">
