210e90c2e3c9dcc79549b680cdacb45c8aac1cef
Hudaismail46 <Hudaismail99@gmail.com>
2020-10-22 16:17:49 +0800

    Refactor routes

--- resources/views/booking/form.blade.php
+++ resources/views/booking/form.blade.php
@@ -3,7 +3,7 @@
         <label class="label" for="event_title">Event Title </label>
         <div class="form-group">
             @csrf
-            <input class="input @error('event_title') is-danger @enderror" type="text" name="event_title" id="event_title" value="{{old('event_title')?? $bookings->event_title ?? ''}}"
+            <input class="input @error('event_title') is-danger @enderror" type="text" name="event_title" id="event_title" value="{{old('event_title')?? $booking->event_title ?? ''}}"
             placeholder=" Event Title">
             <p class="help is-danger">{{ $errors->first('event_title')}}</p>
         </div>
@@ -12,7 +12,7 @@
     <div class="field" >
         <label class="label" for="address">Address </label>
         <div class="form-group">
-            <input class="input @error('address') is-danger @enderror" type="text" name="address" id="address" value="{{old('address')?? $bookings->address ?? ''}}"
+            <input class="input @error('address') is-danger @enderror" type="text" name="address" id="address" value="{{old('address')?? $booking->address ?? ''}}"
             placeholder=" Booking Address">
             <p class="help is-danger">{{ $errors->first('address')}}</p>
         </div>
@@ -21,7 +21,7 @@
     <div class="field">
         <label class="label" for="event_begins">Event Begins </label>
         <div class="form-group">
-            <input class="input @error('date') is-danger @enderror" type="dateTime" name="event_begins" value="{{old('event_begins')?? $bookings->event_begins ?? ''}}"
+            <input class="input @error('date') is-danger @enderror" type="dateTime" name="event_begins" value="{{old('event_begins')?? $booking->event_begins ?? ''}}"
             placeholder=" Month D, Yr HH:MM XM">
             <p class="help is-danger">{{ $errors->first('event_begins')}}</p>
         </div>
@@ -30,7 +30,7 @@
     <div class="field">
         <label class="label" for="event_ends" >Event Ends</label>
         <div class="form-group">
-            <input class="input @error('date') is-danger @enderror" type="dateTime" name="event_ends" value="{{old('event_ends')?? $bookings->event_ends ?? ''}}"
+            <input class="input @error('date') is-danger @enderror" type="dateTime" name="event_ends" value="{{old('event_ends')?? $booking->event_ends ?? ''}}"
             placeholder=" Month D, Yr HH:MM XM">
             <p class="help is-danger">{{ $errors->first('event_begins')}}</p>
         </div>
@@ -39,7 +39,7 @@
     <div class="field">
         <label class="label" for="description">Description </label>
         <div class="control">
-            <input class="input @error('description') is-danger @enderror" type="text" name="description" id="description" value="{{old('description')?? $bookings->description ?? ''}}"
+            <input class="input @error('description') is-danger @enderror" type="text" name="description" id="description" value="{{old('description')?? $booking->description ?? ''}}"
             placeholder=" Event Description">
             <p class="help is-danger">{{ $errors->first('description')}}</p>
         </div>
@@ -48,7 +48,7 @@
     <div class="field">
         <label class="label" for="team">Team</label>
         <div class="control">
-            <input class="input @error('team') is-danger @enderror" type="text" name="team" id="event_title" value="{{old('team')?? $bookings->team ?? ''}}"
+            <input class="input @error('team') is-danger @enderror" type="text" name="team" id="event_title" value="{{old('team')?? $booking->team ?? ''}}"
             placeholder=" Team Name">
             <p class="help is-danger">{{ $errors->first('team')}}</p>
         </div>
