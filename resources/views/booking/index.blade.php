37603514a36f718082e39c1281ddb52b2a646820
syafiqfaiz <syafiqfaiz@gmail.com>
2020-11-17 10:14:10 +0800

    remove single use button

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -19,13 +19,6 @@
 
     <div class="content">
         <div class="container-fluid">
-            @can('import bookings')
-                <div class="row">
-                    <a href={{route('booking.import.new')}}>
-                        <button class="btn-primary btn btn-block col-15 ml-auto">Import</button>
-                    </a>
-                </div>
-            @endcan
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card mt-4">
