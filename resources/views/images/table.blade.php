e03ae62394a5fd826229fa536cf9ea8c1444a85f
syafiqfaiz <syafiqfaiz@gmail.com>
2020-11-24 12:22:15 +0800

    improve order show page

--- resources/views/images/table.blade.php
+++ resources/views/images/table.blade.php
@@ -1,16 +1,28 @@
-@foreach ($images as $image)
-    <img src="{{ asset('/storage/' . $image->file ?? '') }}" alt=""
-        class="img-thumbnail">
-    <p>{{$image->caption}}</p>
-    @if ($image != null)
-        <form class='mb-0' action="{{ route('image.destroy') }}"
-            method="post">
-            @csrf
-            @method('DELETE')
-            <input type="hidden" name='image_id' value="{{$image->id}}">
-            <button class="btn btn-danger mr-2" onclick="return confirm('Are you sure?')"
-                type="submit">Delete Image <i class="fa fa-trash"></i>
-            </button>
-        </form>
-    @endif
-@endforeach
+<div>
+    @foreach ($images as $image)
+        <div class="row mt-2">
+            <div class="col-8">
+                <a href="{{ asset('/storage/' . $image->file) }}" target="_blank">
+                    <img src="{{ asset('/storage/' . $image->file) }}"
+                    alt=""
+                    class="img-thumbnail img-responsive">
+                </a>
+            </div>
+            <div class='col-4'>
+                <div class="d-flex flex-column my-auto">
+                    <p>{{$image->caption}}</p>
+                    <br>
+                    <form class='mb-0' action="{{ route('image.destroy') }}"
+                        method="post">
+                        @csrf
+                        @method('DELETE')
+                        <input type="hidden" name='image_id' value="{{$image->id}}">
+                        <button class="btn btn-danger mt-auto" onclick="return confirm('Are you sure?')"
+                            type="submit">Delete <i class="fa fa-trash"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
