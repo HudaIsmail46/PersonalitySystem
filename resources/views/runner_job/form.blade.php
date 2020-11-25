e455f759c4c428942b828fd38d4c33183baae670
Hudaismail46 <Hudaismail99@gmail.com>
2020-11-25 11:49:23 +0800

    Fixed button complete

--- resources/views/runner_job/form.blade.php
+++ resources/views/runner_job/form.blade.php
@@ -2,6 +2,9 @@
     <tr>
         <tfoot>
             <td>
+                <form action="{{ route('runner_job.complete', $runnerJob->id) }}" method="post">
+                    @csrf
+                    @method('PUT')
                 <button class="btn btn-success mr-2" type="submit">Complete</button>
                 </form>
             </td>
