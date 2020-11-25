675a024462ed4cec3fe15f7997411d4121731355
syafiqfaiz <syafiqfaiz@gmail.com>
2020-11-25 12:46:25 +0800

    overall ui improvement on dropzone

--- resources/views/images/create.blade.php
+++ resources/views/images/create.blade.php
@@ -5,6 +5,7 @@
     data-imageableid="{{$imageableId}}"
     data-imageabletype="{{$imageableType}}">
 </div>
+<div class="btn btn-primary mt-2" id="addMoreFile">Select file</div>
 
 <div class="dz-preview dz-file-preview d-none" id="dzPreview" >
     <div class="dz-details d-flex">
@@ -12,15 +13,13 @@
         <div class='mt-auto ml-3'>
             <div class="dz-filename"><span data-dz-name></span><small><span class="dz-size ml-2" data-dz-size></span></small></div>
             <input type="text" placeholder='Image Caption' name='caption' id='caption' class='w-100'>
+            <p id="imageCaption"></p>
             <div class="mb-1 mt-2 buttons">
                 <div class="btn btn-primary upload">Upload</div>
                 <div class="btn btn-danger cancel">Cancel</div>
             </div>
-            
+            <div class="dz-success-mark d-none"><i class="fas fa-check text-green"></i></div>
+            <div class="dz-error-message text-danger"><span data-dz-errormessage></span></div>
         </div>
     </div>
-    <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
-    <div class="dz-success-mark"><span>✔</span></div>
-    <div class="dz-error-mark"><span>✘</span></div>
-    <div class="dz-error-message"><span data-dz-errormessage></span></div>
 </div>
