30592b279ab54bdfdbf18192302bce917cba275e
Hudaismail46 <Hudaismail99@gmail.com>
2020-11-30 21:01:10 +0800

    Runner show breadcrumb

--- resources/views/runner_job/show.blade.php
+++ resources/views/runner_job/show.blade.php
@@ -14,8 +14,13 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item"><a href="/runner_schedule/{{ $runnerJob->runnerSchedule->id }}">Runner
-                                Schedule</a></li>
+                        @hasrole('Runner')
+                        <li class="breadcrumb-item"><a href ="{{ route('runner.show', $runnerJob->runnerSchedule->id ) }}">Runner
+                            Schedule</a></li>  
+                        @else
+                        <li class="breadcrumb-item"><a href ="{{ route('runner_schedule.show', $runnerJob->runnerSchedule->id ) }}">Runner
+                            Schedule</a></li>
+                        @endhasrole
                         <li class="breadcrumb-item active">Runner Job</li>
                     </ol>
                 </div>
