d9cc0c6c984129fcac02487c5368c8cd5cc1b3fb
Naim <mnaim487@gmail.com>
2020-12-01 16:52:45 +0800

    dateTime format

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -1,9 +1,9 @@
-import dateFormat from 'dateformat';
+import dateFormat  from 'dateformat';
 require('dateformat');
 
 export const dateFormatter = (dateTime) => {
     return (
-        dateFormat(dateTime, "isoDateTime").substring(0, 19)
+        dateFormat(dateTime, " yyyy-mm-dd HH:MM").substring(0, 19)
     );
 }
 
