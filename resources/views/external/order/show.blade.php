89f14f547e79824f4062bf9324913450741660aa
syafiqfaiz <syafiqfaiz@gmail.com>
2020-12-03 17:44:18 +0800

    fixed vendor breadcrumb

--- resources/views/external/order/show.blade.php
+++ resources/views/external/order/show.blade.php
@@ -14,7 +14,8 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Orders</li>
+                        <li class="breadcrumb-item"><a href="{{route('external.order.index')}}">Orders</a></li>
+                        <li class="breadcrumb-item active">Order</li>
                     </ol>
                 </div>
             </div>
