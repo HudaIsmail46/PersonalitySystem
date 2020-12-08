15bdf7d646324665a5607cff847bb1f375698d8f
syafiqfaiz <syafiqfaiz@gmail.com>
2020-12-08 15:39:02 +0800

    updated webhook

--- app/Webhooks/Woocommerce/WebhookHandler.php
+++ app/Webhooks/Woocommerce/WebhookHandler.php
@@ -14,7 +14,7 @@ class WebhookHandler
         $payload = $data["payload"];
         $billing = $payload["billing"];
         $item = $payload["line_items"][0];//for now we only process pnd, the cart is not mixed with other item
-        if($item['product_id'] == "6989120000408"){//hardcoded for now, this is referring for pnd product only
+        if($item['product_id'] == env('PRODUCT_ID', "601294001492")){//hardcoded for now, this is referring for pnd product only
             $customer = Customer::findOrCreate($billing["first_name"]." ".$billing['last_name'], $billing["phone"]);
 
             $order = new Order;
@@ -22,13 +22,13 @@ class WebhookHandler
             $order->fill([
                 'woocommerce_order_id' => $payload["id"],
                 'quantity'=> $item["quantity"],
-                'material' => strtolower(static::valueByKey('carpet-materials', $item['meta_data'])),
-                'size' => strtolower(static::valueByKey('carpet-sizes', $item['meta_data'])),
+                'material' => strtolower(static::valueByKey('carpet-material', $item['meta_data'])),
+                'size' => static::getOrdersize($item['meta_data']),
                 'price' => $item["total"]*100,
                 'prefered_pickup_datetime' => static::preferedDateTime($payload["meta_data"]),
                 'address_1' => $billing["address_1"],
-                'address_2' => $billing["address_2"] ,
-                'postcode' => $billing["postcode"] ,
+                'address_2' => $billing["address_2"],
+                'postcode' => $billing["postcode"],
                 'city' => $billing["city"] ,
                 'location_state' => $billing["state"],
                 'customer_id' => $customer->id,
@@ -41,6 +41,18 @@ class WebhookHandler
 
 
        http_response_code(200);
+    }
+
+    public static function getOrderSize($data)
+    {
+        $size = strtolower(static::valueByKey('carpet-size', $data));
+        if (preg_match('/large/', $size)){
+            return 'l';
+        } elseif (preg_match('/medium/', $size)){
+            return 'm';
+        } else {
+            return 's';
+        }
     }
 
     public static function preferedDateTime($data)
