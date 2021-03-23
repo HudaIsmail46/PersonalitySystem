f0c333505bc009d85f44e80070c6f4f906082ffc
syafiqfaiz <syafiqfaiz@gmail.com>
2021-03-23 11:04:48 +0800

    updated order calculator

--- resources/js/calculateSize.js
+++ resources/js/calculateSize.js
@@ -1,71 +1,79 @@
 
-calculate = function (cal_length,cal_width, total_length,actual_size,type_material,act_price)
+calculate = function ()
 {
-    var length = document.getElementById(cal_length).value;
-    var width = document.getElementById(cal_width).value;
-    var material =document.getElementById(type_material).value;
-    var total =parseInt(length)*parseInt(width);
+    var length = document.getElementById('cal_length').value;
+    var width = document.getElementById('cal_width').value;
+    var material = document.getElementById('type_material').value;
+    var walk_in = document.getElementById('walk_in').checked;
+    var total = parseInt(length)*parseInt(width);
+    var size;
+    var price
 
+    if(total < 35){
+        size = 'XS';
+        if(material == "wool" || material =="silk" || material =="shaggy" || material =="cotton"){
+            if (walk_in) {
+                price = 55;
+            } else {
+                price = 85;
+            }
+        } else {
+            if (walk_in) {
+                price = 35;
+            } else {
+                price = 50;
+            }
+        }
 
-    if(total < 35)
-    {
-        if(material == "wool" || material =="silk" || material =="shaggy" || material =="cotton")
-        {
-            document.getElementById(total_length).innerHTML =total + " ft";
-            document.getElementById(actual_size).innerHTML = "XS";
-            document.getElementById(act_price).innerHTML="RM " +85;
+    } else if(total < 48){
+        size = 'S';
+        if(material == "wool" || material =="silk" || material =="shaggy" || material =="cotton"){
+            if (walk_in) {
+                price = 95;
+            } else {
+                price = 120;
+            }
+        } else {
+            if (walk_in) {
+                price = 65;
+            } else {
+                price = 85;
+            }
         }
-        else
-        {
-            document.getElementById(total_length).innerHTML =total + " ft";
-            document.getElementById(actual_size).innerHTML = "XS";
-            document.getElementById(act_price).innerHTML="RM " +50;
+    } else if(total< 63){
+        size = 'M';
+        if(material == "wool" || material =="silk" || material =="shaggy" || material =="cotton"){
+            if (walk_in) {
+                price = 140;
+            } else {
+                price = 175;
+            }
+        } else {
+            if (walk_in) {
+                price = 95;
+            } else {
+                price = 120;
+            }
         }
+    } else {
+        size = 'L';
+        if(material == "wool" || material =="silk" || material =="shaggy" || material =="cotton"){
+            if (walk_in) {
+                price = 210;
+            } else {
+                price = 280;
+            }
+        } else {
+            if (walk_in) {
+                price = 135;
+            } else {
+                price = 200;
+            }
+        }
+    }
 
-    }
-    else if(total < 48)
-    {
-        if(material == "wool" || material =="silk" || material =="shaggy" || material =="cotton")
-        {
-            document.getElementById(total_length).innerHTML =total + " ft";
-            document.getElementById(actual_size).innerHTML = "S";
-            document.getElementById(act_price).innerHTML="RM " +120;
-        }
-        else
-        {
-            document.getElementById(total_length).innerHTML =total + " ft";
-            document.getElementById(actual_size).innerHTML = "S";
-            document.getElementById(act_price).innerHTML="RM " +85;
-        }
-    }
-    else if(total< 63)
-    {
-        if(material == "wool" || material =="silk" || material =="shaggy" || material =="cotton")
-        {
-            document.getElementById(total_length).innerHTML =total + " ft";
-            document.getElementById(actual_size).innerHTML = "M";
-            document.getElementById(act_price).innerHTML="RM" +175;
-        }
-        else
-        {
-            document.getElementById(total_length).innerHTML =total + " ft";
-            document.getElementById(actual_size).innerHTML = "M";
-            document.getElementById(act_price).innerHTML="RM " +120;
-        }
-    }
-    else{
-        if(material == "wool" || material =="silk" || material =="shaggy" || material =="cotton")
-        {
-            document.getElementById(total_length).innerHTML =total + " ft";
-            document.getElementById(actual_size).innerHTML = "L";
-            document.getElementById(act_price).innerHTML="RM " +280;
-        }
-        else
-        {
-            document.getElementById(total_length).innerHTML =total + " ft";
-            document.getElementById(actual_size).innerHTML = "L";
-            document.getElementById(act_price).innerHTML="RM " +200;
-        }
-    }
+    document.getElementById('total_length').innerHTML = total + " ft";
+    document.getElementById('actual_size').innerHTML = size;
+    document.getElementById('act_price').innerHTML="RM " + price;
 
 }
--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -224,63 +224,7 @@
                     </div>
                 </div>
             </div>
-            <div class ="col-md-4 mx-4 mt-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="mb-0">Calculate Size</h4>
-                    </div>
-                    <div class="card-body">
-
-                        <div class="field">
-                            <label class="label" for="material">Material</label>
-                            <div class="form-group row">
-                                <div class="col-sm-5">
-                                    <select id="type_material" name="type_material" onblur="calculate('cal_length', 'cal_width', 'total_length','actual_size','type_material','act_price')"
-                                        class="form-control">
-                                        <option value="">--SELECT MATERIAL--</option>
-                                        @foreach (App\Order::MATERIALS as $type_material)
-                                            <option value="{{ $type_material }}">{{ $type_material }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="field">
-                            <label class="label" for="cal_length">Actual length (ft) </label>
-                            <div class="form-group row">
-                                <div class="col-auto">
-                                    <input class="form-control "
-                                        type="number" name="cal_length" id="cal_length"
-                                        value="{{ old('cal_length') ?? ($order ?? ''->cal_length ?? '') }}"
-                                        placeholder="Length"
-                                        onblur="calculate('cal_length', 'cal_width', 'total_length','actual_size','type_material','act_price')" >
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="field">
-                            <label class="label" for="cal_width">Actual width (ft) </label>
-                            <div class="form-group row">
-                                <div class="col-auto">
-                                    <input class="form-control "
-                                        type="number" name="cal_width" id="cal_width"
-                                        value="{{ old('cal_width') ?? ($order ?? ''->cal_width ?? '') }}"
-                                        placeholder=" Width"
-                                        onblur="calculate('cal_length', 'cal_width', 'total_length', 'actual_size','type_material','act_price')" >
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="field">
-                            <label class="label" for="total_length">Total Size(ft)</label>
-                            <p id="total_length"></p>
-                            <p id="actual_size"></p>
-                            <p id="act_price"></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('order.price_calculator')
         </div>
     </div>
 </div>
